ccfac9f7174587d319d171257cf0fbd1397f1fd2
Kyle <kwikness@gmail.com>
2023-06-21 18:17:59 -0400

    init

---
+++ simple_random_walk.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import random
+from random import choice
+
+random.seed()
+
+for i in range(1, 100):
+    x = []
+    y = []
+    for j in range(0, 1000000):
+        last_y = 0
+        if j > 1:
+            last_y = y[j-1]
+
+        random_step = choice([k for k in range(-1,2) if k != 0])
+
+        x.append(j)
+        y.append(last_y + random_step)
+
+    plt.plot(x, y)
+
+plt.xlabel('x axis')
+plt.ylabel('y axis')
+plt.title('Simple Random Walk')
+plt.show()
